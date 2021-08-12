4f761ff7605674c6bd0840f2b4c2ffe6812303ce
Shiloli-r <ronaldshiloli@gmail.com>
2021-08-12 18:46:08 +0300

    Product model

--- farmblr/products/models.py
+++ farmblr/products/models.py
@@ -1,8 +1,16 @@
+import os
 from django.db import models
+from uuid import uuid4
+
+
+def get_unique_path(_, filename):
+    """create a unique path for the file to be uploaded"""
+    extension = filename.split('.', 1)[-1]
+    filename = "{}.{}".format(uuid4(), extension)
+    return os.path.join('products', filename)
 
 
 # Create your models here.
-
 class Category(models.Model):
     category = models.CharField(max_length=250)
 
@@ -11,3 +19,14 @@ class Category(models.Model):
 
     class Meta:
         verbose_name_plural = 'Categories'
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    product_name = models.CharField(max_length=250)
+    description = models.TextField()
+    price = models.IntegerField()
+    photo = models.ImageField(upload_to=get_unique_path)
+
+    def __str__(self):
+        return "{}".format(self.product_name)
