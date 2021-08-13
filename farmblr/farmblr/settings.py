f4a0ce32256dfc4a32f3fc654cb42397c604d27c
Shiloli-r <ronaldshiloli@gmail.com>
2021-08-13 22:13:49 +0300

    user uploaded images

--- farmblr/farmblr/settings.py
+++ farmblr/farmblr/settings.py
@@ -126,6 +126,10 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
+#  User uploaded files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
