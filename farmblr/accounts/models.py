7afcdf5c58f82edd51cec7027fc01372b45f3762
Shiloli-r <ronaldshiloli@gmail.com>
2021-08-19 19:03:27 +0300

    Profile model

--- farmblr/accounts/models.py
+++ farmblr/accounts/models.py
@@ -1,3 +1,20 @@
+import os
+from uuid import uuid4
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    gender = models.CharField(max_length=25, null=True)
+    date_of_birth = models.DateField(null=True, blank=True)
+    id_number = models.IntegerField()
+    country = models.CharField(max_length=150)
+    mobile = models.CharField(max_length=50, null=True, blank=True)
+    street = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    postal_code = models.CharField(max_length=100, null=True)
+
+    def __str__(self):
+        return self.user.username
--- farmblr/farmblr/settings.py
+++ farmblr/farmblr/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'web',
     'blog',
+    'accounts',
     'products'
 ]
 
