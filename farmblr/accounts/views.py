735e7ea4470e0c6e572760e592510ae074ef08fb
Shiloli-r <ronaldshiloli@gmail.com>
2021-08-26 19:55:30 +0300

    log in view

--- farmblr/accounts/views.py
+++ farmblr/accounts/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+
+from .forms import LoginForm
+
 
 # Create your views here.
+def login_(request):
+    if request.user.is_authenticated:
+        pass
+        # TODO Redirect to Dashboard
+    next_ = request.GET.get('next')
+    # login form
+    log_in = LoginForm(request.POST or None)
+    if log_in.is_valid():
+        username = log_in.cleaned_data.get('username')
+        password = log_in.cleaned_data.get('password')
+        user = authenticate(username=username, password=password)
+        login(request, user)
+        if next_:
+            redirect(next_)
+        return redirect('dashboard')
+    context = {
+        'log_in': log_in,
+    }
+    return render(request, 'accounts/login.html', context)
