6427df3caf0c25f9b4545c57a37be2caf076c6c7
Shiloli-r <ronaldshiloli@gmail.com>
2021-08-29 17:49:01 +0300

    Category model

--- farmblr/products/models.py
+++ farmblr/products/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Category(models.Model):
+    category = models.CharField(max_length=250)
+
+    def __str__(self):
+        return '{}'.format(self.category)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
