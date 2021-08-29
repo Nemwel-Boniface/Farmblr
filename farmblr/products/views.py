41108bac66e360f9a3bd159dbf085d4338373eb8
Shiloli-r <ronaldshiloli@gmail.com>
2021-08-29 17:36:17 +0300

    moved marketplace to products

--- farmblr/products/views.py
+++ farmblr/products/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def marketplace(request):
+    return render(request, 'products/marketplace.html')
--- farmblr/web/urls.py
+++ farmblr/web/urls.py
@@ -4,5 +4,4 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('marketplace', views.marketplace, name='marketplace'),
 ]
