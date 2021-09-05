f21ece8a55122707a6945307b54c7e4d58ecc1aa
Shiloli-r <ronaldshiloli@gmail.com>
2021-09-06 00:22:43 +0300

    Blog url

--- farmblr/farmblr/urls.py
+++ farmblr/farmblr/urls.py
@@ -22,7 +22,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('web.urls')),
     path('marketplace/', include('products.urls')),
-    path('accounts/', include('accounts.urls'))
+    path('accounts/', include('accounts.urls')),
+    path('blog/', include('blog.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
