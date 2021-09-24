96dc7d0671659702f6c084fe35ade1ad4ff7ebde
Shiloli-r <ronaldshiloli@gmail.com>
2021-09-24 23:20:57 +0300

    Profile urls

--- farmblr/accounts/urls.py
+++ farmblr/accounts/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('login', views.login_, name='login'),
     path('logout', views.logout, name='logout'),
     path('signup', views.sign_up, name='signup'),
+    path('profile/<slug:usernmae>', views.profile, name='profile'),
 ]
