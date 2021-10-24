b6e69fcd72d3ddb3fde3d0b328dd9f942aee33b9
Shiloli-r <ronaldshiloli@gmail.com>
2021-10-24 12:11:50 +0300

    Admin Blog post modification

--- farmblr/blog/admin.py
+++ farmblr/blog/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
 from .models import Post
 
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'status', 'created_on', 'updated_on')
+    list_filter = ("status",)
+    search_fields = ['title', 'content']
+    prepopulated_fields = {'slug': ('title',)}
+
+
 # Register your models here.
-admin.site.register(Post)
+admin.site.register(Post, PostAdmin)
