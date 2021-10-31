c7484146e6e9a42de214d788a531f383f7f087c2
Shiloli-r <ronaldshiloli@gmail.com>
2021-10-31 12:13:27 +0300

    Admin Blog comment

--- farmblr/blog/admin.py
+++ farmblr/blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
 
 class PostAdmin(admin.ModelAdmin):
@@ -9,5 +9,16 @@ class PostAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
 
 
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'body', 'post', 'created_on', 'active')
+    list_filter = ('active', 'created_on')
+    search_fields = ('name', 'email', 'body')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(active=True)
+
+
 # Register your models here.
 admin.site.register(Post, PostAdmin)
+admin.site.register(Comment, CommentAdmin)
