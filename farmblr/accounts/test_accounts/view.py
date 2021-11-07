7136e80281e87d3ce9e022627c7aa22cdceec4da
Shiloli-r <ronaldshiloli@gmail.com>
2021-11-07 12:57:51 +0300

    Test for login view

---
+++ farmblr/accounts/test_accounts/view.py
@@ -0,0 +1,21 @@
+from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth.models import User
+
+from model_bakery import baker
+from ..models import Profile
+
+class TestAccountViews(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        credentials = {
+            "username": "test_user",
+            "password": "test_password",
+        }
+        User.objects.create_user(**credentials)
+
+    def test_login__view(self):
+        response = self.client.get(reverse('login'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'accounts/login.html')
+
