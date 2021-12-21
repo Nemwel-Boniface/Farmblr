13643005218e38375c41e751178a5901b984046b
Shiloli-r <ronaldshiloli@gmail.com>
2021-12-22 00:58:11 +0300

    Username validation

--- farmblr/static/js/accounts/validation.js
+++ farmblr/static/js/accounts/validation.js
@@ -6,3 +6,24 @@ const password1_input = document.querySelector("#id_password1");
 const password2_input = document.querySelector("#id_password2");
 const passwords_error_area = document.querySelector(".password-errors");
 
+/* Username validation */
+username_input.addEventListener('keyup', (e) => {
+    const usernameValue = e.target.value;
+    if (usernameValue.length > 0){
+        fetch("/accounts/validateUsername", {
+            body: JSON.stringify({ username: usernameValue }),
+            method: "POST",
+        })
+            .then((res)=>res.json())
+            .then((data) => {
+            if(data.username_error){
+                username_input.classList.add("invalid");
+                username_error_area.style.display = 'block';
+                username_error_area.innerHTML = `<p>${data.username_error}</p>`
+            }else {
+                username_input.classList.remove('invalid')
+                username_error_area.style.display = 'none';
+            }
+        });
+    }
+});
