637718c7f73b00ff3a817cce520b52562bcf5c3e
Shiloli-r <ronaldshiloli@gmail.com>
2021-12-30 00:57:11 +0300

    form input content

---
+++ farmblr/static/js/accounts/validation.js
@@ -0,0 +1,8 @@
+const username_input = document.querySelector("#id_username");
+const email_input = document.querySelector('#id_email');
+const username_error_area = document.querySelector(".username-errors");
+const email_error_area = document.querySelector(".email-errors");
+const password1_input = document.querySelector("#id_password1");
+const password2_input = document.querySelector("#id_password2");
+const passwords_error_area = document.querySelector(".password-errors");
+
